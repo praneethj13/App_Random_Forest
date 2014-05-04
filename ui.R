1e25fd484683495c4415d093bfda8292b6258aeb
praneethj13 <praneeth.jagarapu445@gmail.com>
2014-05-04 22:29:08 +0530

    Adding Simulator

--- ui.R
+++ ui.R
@@ -6,10 +6,9 @@ shinyUI(
   navbarPage(
     theme = "bootstrap.css",
     "Random Forest !!", # App Heading
-    
     source("ui_Tab1_Load Data.R", local=TRUE)$value,
-    source("ui_Tab2_Classification.R", local=TRUE)$value
-    
+    source("ui_Tab2_Classification.R", local=TRUE)$value,
+    source("ui_Tab3_Simulator.R", local=TRUE)$value        
   )
 )
 
